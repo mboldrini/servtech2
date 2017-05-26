2144b6fccd6343347c3c5b6f26635d465fb235fa
Matheus Boldrini <matheus.souza.boldrini@hotmail.com>
2017-05-26 13:38:26 -0300

    alteracoes esteticas
    inclusao de uma verificacao do primeiro segmento da url para colocar a
    classe active no menu

--- application/views/base/sidebar.php
+++ application/views/base/sidebar.php
@@ -7,21 +7,49 @@
       <!-- Sidebar Menu -->
       <ul class="sidebar-menu">
 
+        <?php 
+        	$paginaAtual = $this->uri->segment(1); 
+        	if( $paginaAtual  == 'painel')			{ $pgPainel 		= "active"; }
+        	else if( $paginaAtual == 'cliente')		{ $pgClientes 		= "active"; }
+        	else if( $paginaAtual == 'servico')		{ $pgServicos 		= "active"; }
+        	else if( $paginaAtual == 'tiposervico')	{ $pgTipoServico 	= "active"; }
+        	else if( $paginaAtual == 'valorservico'){ $pgValorServico 	= "active"; }
+        	else if( $paginaAtual == 'mensagens')	{ $pgMensagens 		= "active"; }
+        	else if( $paginaAtual == 'contas')		{ $pgContas 		= "active"; }
+        	else if( $paginaAtual == 'perfil')		{ $pgPerfil 		= "active"; }
+        ?>
+
         <li class="header">Menu</li>
-        <li><a href="<?= base_url(); ?>painel"><i class="fa fa-dashboard"></i> <span>Painel</span></a></li>
+	        <li class="<?php if( isset($pgPainel) && $pgPainel != NULL){ echo"active";} ?>">
+	        	<a href="<?= base_url(); ?>painel"><i class="fa fa-dashboard"></i> <span>Painel</span></a>
+	        </li>
 
         <li class="header">Front Office</li>
-        <li><a href="<?= base_url(); ?>cliente"><i class="fa fa-user"></i> <span>Clientes</span></a></li>
-        <li><a href="<?= base_url(); ?>servico"><i class="fa fa-gears"></i> <span>Serviços</span></a></li>
+	        <li class="<?php if( isset($pgClientes) && $pgClientes != NULL){ echo"active";} ?>">
+	        	<a href="<?= base_url(); ?>cliente"><i class="fa fa-user"></i> <span>Clientes</span></a>
+	        </li>
+	        <li class="<?php if( isset($pgServicos) && $pgServicos != NULL){ echo"active";} ?>">
+	        	<a href="<?= base_url(); ?>servico"><i class="fa fa-gears"></i> <span>Serviços</span></a>
+	        </li>
 
         <li class="header">Financeiro</li>
-        <li><a href="<?= base_url(); ?>tiposervico"><i class="fa fa-book"></i> <span>Tipo de Serviço</span></a></li>
-        <li><a href="<?= base_url(); ?>valorservico"><i class="fa fa-usd"></i> <span>Valor do Serviço</span></a></li>
+        	<li class="<?php if( isset($pgTipoServico) && $pgTipoServico != NULL){ echo"active";} ?>">
+        		<a href="<?= base_url(); ?>tiposervico"><i class="fa fa-book"></i> <span>Tipo de Serviço</span></a>
+        	</li>
+        	<li class="<?php if( isset($pgValorServico) && $pgValorServico != NULL){ echo"active";} ?>">
+        		<a href="<?= base_url(); ?>valorservico"><i class="fa fa-usd"></i> <span>Valor do Serviço</span></a> 
+        	</li>
         
         <li class="header">Administrativo</li>
-        <li><a href="<?= base_url(); ?>mensagens"><i class="fa fa-comment"></i> <span>Mensagens/Avisos</span></a></li>
-        <li><a href="<?= base_url(); ?>contas"><i class="fa fa-users"></i> <span>Usuários</span></a></li>
-        <li><a href="<?= base_url(); ?>perfil"><i class="fa fa-user"></i> <span>Meu Perfil</span></a></li>
+        	<li class="<?php if( isset($pgMensagens) && $pgMensagens != NULL){ echo"active";} ?>">
+        		<a href="<?= base_url(); ?>mensagens"><i class="fa fa-comment"></i> <span>Mensagens/Avisos</span></a>
+        	</li>
+        	<li class="<?php if( isset($pgContas) && $pgContas != NULL){ echo"active";} ?>"> 
+        		<a href="<?= base_url(); ?>contas"><i class="fa fa-users"></i> <span>Usuários</span></a>
+        	</li>
+        	<li class="<?php if( isset($pgPerfil) && $pgPerfil != NULL){ echo"active";} ?>">
+        		<a href="<?= base_url(); ?>perfil"><i class="fa fa-user"></i> <span>Meu Perfil</span></a>
+        	</li>
         
       </ul>
       <!-- /.sidebar-menu -->
