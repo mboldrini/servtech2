0287bb7ed6a731df40024d126727b9ae62ccda30
Matheus Boldrini <matheus.souza.boldrini@hotmail.com>
2017-05-26 09:23:48 -0300

    cadastro de valor de servico

--- application/controllers/Valorservico.php
+++ application/controllers/Valorservico.php
@@ -31,7 +31,7 @@ class Valorservico extends CI_Controller {
 		$dados = array(
 			'pasta'		=>	'valorservico',
 			'tela'		=>	'listar',
-			'titulo'	=>	'Perfil do Usuário',
+			'titulo'	=>	'Valores de Serviços',
 			'infos'		=>	$pegaInfos,
 			'valores'	=>  $this->funcoes->getAll('valser'),
 			'tipoServico'=> $this->funcoes->getAll('tipser'),
@@ -40,7 +40,73 @@ class Valorservico extends CI_Controller {
 
 		$this->load->view('tela',$dados);
 
-	}//index
+	}
+
+	public function cadastrar()	{
+
+		# verificação de usuario logado, e se sim, tem que ser no perfil de administrador
+		if($this->session->userdata('perfil') == FALSE || $this->session->userdata('perfil') != 'administrador'){
+			redirect(base_url().'login');
+		}
+
+		# pega o nome do usuario que tem na session e passa >
+		$nome = $this->session->userdata('username');
+		# pega o nome da variavel aqui de cima, e faz uma pesquisa completa no banco de dados 'user'
+		$pegaInfos = $this->usuario->pegaUsuario($nome);
+
+		$mensagem = [];
+
+		$this->form_validation->set_rules('tipo', 		'Tipo de Serviço', 	'trim|required');
+		$this->form_validation->set_rules('valor', 		'Valor', 			'trim|required');
+		$this->form_validation->set_rules('inival', 	'Data de Início', 	'trim|required');
+		$this->form_validation->set_rules('fimVal', 	'Data Final', 		'trim|required');
+		$this->form_validation->set_rules('datCad', 	'Data de Cadastro', 'trim|required');
+		$this->form_validation->set_rules('usuCad', 	'ID do usuário', 	'trim|required');
+
+		$tipo 		= $this->input->post('tipo');
+		$valor 		= $this->input->post('valor');
+		$iniVal 	= $this->input->post('inival');
+		$fimVal 	= $this->input->post('fimVal');
+		$datCad 	= $this->input->post('datCad');
+		$usuCad	 	= $this->input->post('usuCad');
+
+		if( $this->form_validation->run() == FALSE ){
+			if( validation_errors() ){
+				$mensagem[0] = '<strong>Opa!</strong> Algo de errado aconteceu! ' . validation_errors();
+				$mensagem[1] = 'alert-danger';
+			}
+		}else{
+			$registra = array(
+				"tipo" 		=> $tipo,
+				"valor"		=> $valor,
+				"iniVal"	=> $iniVal,
+				"fimVal"	=> $fimVal,
+				"datCad"	=> $datCad,
+				"usuCad"	=> $usuCad,
+			);
+			$this->funcoes->insert($registra, 'valser' );
+			$mensagem[0] = '<strong>Parabéns!</strong> Você cadastrou um aviso/mensagem!';
+			$mensagem[1] = 'alert-success';
+		}
+
+		$dados = array(
+			'pasta'		=>	'valorservico',
+			'tela'		=>	'cadastrar',
+			'titulo'	=>	'Cadastro de Valor',
+			'descricao'	=>  "Tela de cadastro de valor de serviço",
+			'infos'		=>	$pegaInfos,
+			'tipoServico'=> $this->funcoes->getAll('tipser'),
+			'mensagem'	=>  $mensagem,
+		);
+
+		$this->load->view('tela',$dados);
+
+	}
+
+
+
+
+
 
 
 
---
+++ application/views/valorservico/cadastrar.php
@@ -0,0 +1,93 @@
+<div class="row">
+
+<div class="col-md-4">
+
+<?php echo form_open(); ?>
+
+<?php 
+  if( isset( $mensagem ) && $mensagem != null ){
+    echo '<div class="alert '.$mensagem[1].' alert-dismissible" role="alert">
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+  '.$mensagem[0].'</div>';
+  }
+?>
+
+
+	<div class="form-group">
+		<label for="Status">Tipo de Servico:</label>
+		<select name="tipo" id="tipo" class="form-control">
+			<?php foreach( $tipoServico as $servico ): ?>
+				<option value="<?php echo $servico->id; ?>"><?php echo $servico->tipo; ?></option>
+			<?php endforeach; ?>
+		</select>
+	</div>
+
+
+	<div class="form-group">
+	    <label for="exampleInputEmail1">Valor (R$):</label>
+	    <?php echo form_input(
+	      'valor', 
+	      '',
+	      array( 'class'    	=>  'form-control col-md-3',
+	             'required'   	=>  'required',
+	             'placeholder'  =>  'Valor do Serviço',
+	            ) 
+	        ); 
+	    ?>
+  	</div>
+  
+  <div class="form-group">
+    <label for="exampleInputEmail1">Data de Início:</label>
+    <?php echo form_input(
+      'inival', 
+      '',
+      array( 'class'    	=>  'form-control col-md-3',
+             'required'   	=>  'required',
+             'placeholder'  =>  'Data de Início',
+            ) 
+        ); 
+    ?>
+  </div>
+
+  <div class="form-group">
+    <label for="exampleInputEmail1">Data Final:</label>
+    <?php echo form_input(
+      'fimVal', 
+      '',
+      array( 'class'    	=>  'form-control col-md-3',
+             'required'   	=>  'required',
+             'placeholder'  =>  'Data Final',
+            ) 
+        ); 
+    ?>
+  </div>
+
+  	<div class="form-group">
+    	<?php echo form_label('Data de Cadastro:', 'date'); ?>
+    	<?php date_default_timezone_set('America/Sao_Paulo'); $date = date('d-m-Y'); ?>
+    	<?php echo form_input('datCad', $date ,array( 'class'=>'form-control col-md-3', 'required'=>'required', 'readonly'=>'readonly' ) ); ?>
+  	</div>
+	
+
+	<div class="form-group">
+      <?php echo form_label('Usuário responsavel:', 'usuario'); ?>
+      <?php 
+        $opcoes = array(
+          $infos[0]->id => $infos[0]->nome,
+        );
+      ?>
+      <?php  echo form_dropdown('usuCad', $opcoes, '', array('class'=>'form-control', 'readonly'=>'readonly')  ); ?>
+  	</div>
+  
+
+	
+	
+	
+
+
+	<button type="submit" class="btn btn-primary">Cadastrar</button>
+
+<?php form_close(); ?>
+
+	</div><!--col-md-4-->
+</div><!--row-->
