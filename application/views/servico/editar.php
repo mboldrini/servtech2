77f0f742ae985e1548e21c22ba6f3dbb5636b4b1
Matheus Boldrini <matheus.souza.boldrini@hotmail.com>
2017-06-06 11:25:13 -0300

    formulario de servico edicao

--- application/views/servico/editar.php
+++ application/views/servico/editar.php
@@ -1,18 +1,15 @@
 <div class="container-fluid"><div class="row">
 	
-<?php 
-  if( $mensagem ){
-    echo '<div class="alert '.$mensagem[1].' alert-dismissible" role="alert">
-  <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-  '.$mensagem[0].'</div>';
-  }
-?>
-
-<div class="col-md-4">
+	<?php 
+  		if( isset( $mensagem ) && $mensagem != null ){
+    		echo '<div class="alert '.$mensagem[1].' alert-dismissible" role="alert">
+  			<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+  			'.$mensagem[0].'</div>';
+ 		}
+	?>
 
 	<?php echo form_open(); ?>
 
-  	<div class="">
         <div class="box box-primary">
             <div class="box-header with-border">
               <h3 class="box-title">Informações do Cliente</h3>
@@ -22,30 +19,50 @@
               </div>
             </div>
             <div class="box-body">
-				<div class="form-group">
-    				<label for="exampleInputName2">ID do Cliente:</label>
+
+            	<div class="col-md-2">
+            		<label for="exampleInputName2">ID:</label>
     				<input type="text" class="form-control" id="codCli" name="codCli" placeholder="ID" value="<?php echo $editar->codCli; ?>" readonly>
-  				</div>
-				<div class="form-group">
-    				<label for="exampleInputEmail2">Nome:</label>
+            	</div>
+
+				<div class="col-md-4">
+					<label for="exampleInputEmail2">Nome:</label>
     				<?php foreach( $clientes as $cliente ): ?>
     					<?php if( $cliente->id == $editar->codCli ){ ?>
     						<input type="text" class="form-control" id="nomeCliente" placeholder="nome" value="<?php echo $cliente->nome;?>" readonly>
     					<?php } ?>
     				<?php endforeach; ?>
-  				</div>
+				</div>
+
+				<div class="col-md-2">
+					<label for="exampleInputEmail2">Tipo:</label>
+					<?php $tipo = ""; ?>
+					<?php if( $cliente->tipo == "f" || $cliente->tipo == 'F'){ ?>
+						<input type="text" class="form-control" id="tipoCliente" placeholder="nome" value="Pessoa Física" readonly>
+					<?php }else{ ?>
+						<input type="text" class="form-control" id="tipoCliente" placeholder="nome" value="P. Jurídica" readonly>
+					<?php } ?>
+				</div>
+
+				<div class="col-md-4">
+					<label for="exampleInputEmail2">Registro:</label>
+    				<input type="text" class="form-control" id="nomeCliente" placeholder="nome" value="<?php echo $cliente->registro;?>" readonly>
+				</div>
+
             </div>
         </div>
-    </div>
 
     <div class="form-group">
-    	<label for="exampleInputName2">ID do Serviço:</label>
+    	
+  	</div>
+
+  	<div class="col-md-2">
+  		<label for="exampleInputName2">ID do Serviço:</label>
     	<input type="text" class="form-control" id="idSer" name="idSer" placeholder="ID" value="<?php echo $editar->id; ?>" readonly>
   	</div>
 
-  
-	<div class="form-group">
-		<label for="exampleInputEmail1">Tipo de Serviço:</label>
+  	<div class="col-md-4">
+  		<label for="exampleInputEmail1">Tipo de Serviço:</label>
 		<select name="tipo" id="tipo" class="form-control">
 			<?php foreach( $tiposervico as $tipo ): ?>
 				<?php if( $tipo->id == $editar->tipo ){ ?>
@@ -55,49 +72,10 @@
 				<?php } ?>
 			<?php endforeach; ?>
 		</select>
-	</div>
+  	</div>
 
-	<div class="form-group">
-	    <label for="exampleInputEmail1">Solicitado:</label>
-	    <?php echo form_textarea(
-	      'solicitado', 
-	      $editar->solicitado,
-	      array( 'class'    =>  'form-control col-md-3',
-	             'required'   =>  'required',
-	             'placeholder'  =>  'Solicitado',
-	            ) 
-	        ); 
-	    ?>
-	</div>
-
-	<div class="form-group">
-	    <label for="exampleInputEmail1">Detectado:</label>
-	    <?php echo form_textarea(
-	      'detectado', 
-	      $editar->detectado,
-	      array( 'class'    =>  'form-control col-md-3',
-	             'placeholder'  =>  'Detectado',
-	            ) 
-	        ); 
-	    ?>
-	</div>
-
-	<div class="form-group">
-	    <label for="exampleInputEmail1">Previsão de Conclusão:</label>
-	    <?php echo form_input(
-	      'preCon', 
-	      $editar->preCon,
-	      array( 'class'    =>  'form-control col-md-3',
-	             'required'   =>  'required',
-	             'placeholder'  =>  'Previsão de Conclusão',
-	             'data-mask' => 'data-mask',
-             	'data-inputmask' => '\'alias\': \'dd/mm/yyyy\'', 
-	            ) 
-	        ); 
-	    ?>
-	</div>
-
-	<div class="form-group">
+  
+	<div class="col-md-3">
 		<label for="Status">Status:</label>
 		<?php 
 		$cores = array(
@@ -119,32 +97,70 @@
 		</select>
 	</div>
 
-	<div class="form-group">
-	    <label for="exampleInputEmail1">Solução:</label>
-	    <?php echo form_textarea(
-	      'solucao', 
-	      $editar->solucao,
+	<div class="col-md-2">
+		<label for="exampleInputEmail1">Previsão de Conclusão:</label>
+	    <?php echo form_input(
+	      'preCon', 
+	      $editar->preCon,
 	      array( 'class'    =>  'form-control col-md-3',
-	             'placeholder'  =>  'Solucão',
+	             'required'   =>  'required',
+	             'placeholder'  =>  'Previsão de Conclusão',
+	             'data-mask' => 'data-mask',
+             	'data-inputmask' => '\'alias\': \'dd/mm/yyyy\'', 
 	            ) 
 	        ); 
 	    ?>
 	</div>
 
-	<div class="form-group">
-	    <label for="exampleInputEmail1">Observacão:</label>
+	<div class="col-md-12">
+		<label for="exampleInputEmail1">Solicitado:</label>
 	    <?php echo form_textarea(
-	      'observacao', 
-	      $editar->observacao,
+	      'solicitado', 
+	      $editar->solicitado,
 	      array( 'class'    =>  'form-control col-md-3',
-	             'placeholder'  =>  'Observação',
+	             'required'   =>  'required',
+	             'placeholder'  =>  'Solicitado',
 	            ) 
 	        ); 
 	    ?>
 	</div>
 
-	<div class="form-group">
-	    <label for="exampleInputEmail1">Data de Conclusão:</label>
+	<div class="col-md-12">
+		<h3>Parte Técnica</h3>
+	</div>
+	
+	<div class="col-md-6">
+		<label for="exampleInputEmail1">Detectado:</label>
+	    <?php echo form_textarea(
+	      'detectado', 
+	      $editar->detectado,
+	      array( 'class'    =>  'form-control col-md-3',
+	             'placeholder'  =>  'Detectado',
+	            ) 
+	        ); 
+	    ?>
+	</div>
+	
+
+	<div class="col-md-6">
+		<label for="exampleInputEmail1">Solução:</label>
+		    <?php echo form_textarea(
+		      'solucao', 
+		      $editar->solucao,
+		      array( 'class'    =>  'form-control col-md-3',
+		             'placeholder'  =>  'Solucão',
+		            ) 
+		        ); 
+		    ?>
+	</div>
+
+	<div class="col-md-12">
+		<h3>Finalização</h3>
+	</div>
+
+
+	<div class="col-md-3">
+		<label for="exampleInputEmail1">Data de Conclusão:</label>
 	    <?php echo form_input(
 	      'datCon', 
 	      $editar->datCon,
@@ -157,7 +173,7 @@
 	    ?>
 	</div>
 
-	<div class="form-group">
+	<div class="col-md-3">
 		<label for="exampleInputEmail1">Técnico Responsável:</label>
 		<select name="tecRes" id="tecRes" class="form-control">
 			<?php foreach( $usuarios as $usuario ): ?>
@@ -170,8 +186,8 @@
 		</select>
 	</div>
 
-	<div class="form-group">
-	    <label for="exampleInputEmail1">Acréscimo de Valor:</label>
+	<div class="col-md-3">
+		<label for="exampleInputEmail1">Acréscimo de Valor:</label>
 	    <?php echo form_input(
 	      'acrescimo', 
 	      $editar->acrescimo,
@@ -182,8 +198,8 @@
 	    ?>
 	</div>
 
-	<div class="form-group">
-	    <label for="exampleInputEmail1">Desconto de Valor:</label>
+	<div class="col-md-3">
+		<label for="exampleInputEmail1">Desconto de Valor:</label>
 	    <?php echo form_input(
 	      'desconto', 
 	      $editar->desconto,
@@ -194,28 +210,28 @@
 	    ?>
 	</div>
 
-	<div class="form-group">
-    	<?php echo form_label('Data de Cadastro:', 'date'); ?>
-    	<?php echo form_input('datCad', $editar->datCad ,array( 'class'=>'form-control col-md-3', 'required'=>'required', 'readonly'=>'readonly',  'data-mask' => 'data-mask',
-             'data-inputmask' => '\'alias\': \'dd/mm/yyyy\'',  ) ); ?>
-  	</div>
 
-	<div class="form-group">
-    	<label for="exampleInputEmail1">Autor:</label>
-    	<select name="usuCad" id="usuCad" class="form-control" readonly>
-    		<?php foreach( $usuarios as $usuCad ): ?>
-				<?php if( $usuCad->id == $editar->tecRes ){ ?>
-					<option value="<?php echo $usuario->id; ?>" selected><?php echo $usuario->nome ?></option>
-				<?php }?>
-			<?php endforeach; ?>
-    	</select>
-  	</div>
+	<div class="col-md-12">
+		<label for="exampleInputEmail1">Observacão:</label>
+	    <?php echo form_textarea(
+	      'observacao', 
+	      $editar->observacao,
+	      array( 'class'    =>  'form-control col-md-3',
+	             'placeholder'  =>  'Observação',
+	            ) 
+	        ); 
+	    ?>
+	</div>
 
-	<button type="submit" class="btn btn-primary">Editar</button>
+
+	<div class="col-md-12">
+		<br>
+		<button type="submit" class="btn btn-primary">Editar</button>
+	</div>
+
 
 <?php form_close(); ?>
 
 
-</div><!--col-md-4-->
 
 </div></div>
--- application/views/servico/novo.php
+++ application/views/servico/novo.php
@@ -1,16 +1,14 @@
 <div class="container-fluid"><div class="row">
 	
 <?php 
-  if( $mensagem ){
-    echo '<div class="alert '.$mensagem[1].' alert-dismissible" role="alert">
-  <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-  '.$mensagem[0].'</div>';
-  }
+  	if( isset( $mensagem ) && $mensagem != null ){
+    	echo '<div class="alert '.$mensagem[1].' alert-dismissible" role="alert">
+  			<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+  			'.$mensagem[0].'</div>';
+ 	}
 ?>
 
-<div class="col-md-6">
-
-	<?php echo form_open(); ?>
+<?php echo form_open(); ?>
 
 	<div class="">
         <div class="box box-primary">
@@ -22,57 +20,60 @@
               </div>
             </div>
             <div class="box-body">
-				<div class="form-group">
-    				<label for="exampleInputName2">ID do Cliente:</label>
+
+            	<div class="col-md-1">
+            		<label for="exampleInputName2">ID:</label>
     				<input type="text" class="form-control" id="codCli" name="codCli" placeholder="ID" value="<?php echo $cliente->id; ?>" readonly>
-  				</div>
-				<div class="form-group">
-    				<label for="exampleInputEmail2">Nome:</label>
+            	</div>
+
+				<div class="col-md-4">
+					<label for="exampleInputEmail2">Nome:</label>
     				<input type="text" class="form-control" id="nomeCliente" placeholder="nome" value="<?php echo $cliente->nome;?>" readonly>
-  				</div>
+				</div>
+
+				<div class="col-md-2">
+					<label for="exampleInputEmail2">Tipo:</label>
+					<?php $tipo = ""; ?>
+					<?php if( $cliente->tipo == "f" || $cliente->tipo == 'F'){ ?>
+						<input type="text" class="form-control" id="tipoCliente" placeholder="nome" value="Pessoa Física" readonly>
+					<?php }else{ ?>
+						<input type="text" class="form-control" id="tipoCliente" placeholder="nome" value="P. Jurídica" readonly>
+					<?php } ?>
+				</div>
+
+				<div class="col-md-4">
+					<label for="exampleInputEmail2">Registro:</label>
+    				<input type="text" class="form-control" id="nomeCliente" placeholder="nome" value="<?php echo $cliente->registro;?>" readonly>
+				</div>
+
             </div>
         </div>
     </div>
 
 	
-  	
-
-	<div class="form-group">
-		<label for="exampleInputEmail1">Tipo de Serviço:</label>
+  	<div class="col-md-4">
+  		<label for="exampleInputEmail1">Tipo de Serviço:</label>
 		<select name="tipo" id="tipo" class="form-control">
 			<?php foreach( $tiposervico as $tipo ): ?>
 				<option value="<?php echo $tipo->id; ?>"><?php echo $tipo->tipo; ?></option>
 			<?php endforeach; ?>
 		</select>
+  	</div>
+
+
+	<div class="col-md-3">
+		<label for="exampleInputEmail1">Status:</label>
+		<select name="status" id="status" class="form-control">
+			<option value="1">Em Aberto</option>
+			<option value="2">Executando</option>
+			<option value="3">Em Espera</option>
+			<option value="4">Concluído</option>
+			<option value="5">Cancelado</option>
+		</select>
 	</div>
 
-	<div class="form-group">
-	    <label for="exampleInputEmail1">Solicitado:</label>
-	    <?php echo form_textarea(
-	      'solicitado', 
-	      '',
-	      array( 'class'    =>  'form-control col-md-3',
-	             'required'   =>  'required',
-	             'placeholder'  =>  'Solicitado',
-	            ) 
-	        ); 
-	    ?>
-	</div>
-
-	<div class="form-group">
-	    <label for="exampleInputEmail1">Detectado:</label>
-	    <?php echo form_textarea(
-	      'detectado', 
-	      '',
-	      array( 'class'    =>  'form-control col-md-3',
-	             'placeholder'  =>  'Detectado',
-	            ) 
-	        ); 
-	    ?>
-	</div>
-
-	<div class="form-group">
-	    <label for="exampleInputEmail1">Previsão de Conclusão:</label>
+	<div class="col-md-3">
+		<label for="exampleInputEmail1">Previsão de Conclusão:</label>
 	    <?php echo form_input(
 	      'preCon', 
 	      '',
@@ -86,19 +87,38 @@
 	    ?>
 	</div>
 
-	<div class="form-group">
-		<label for="exampleInputEmail1">Status:</label>
-		<select name="status" id="status" class="form-control">
-			<option value="1">Em Aberto</option>
-			<option value="2">Executando</option>
-			<option value="3">Em Espera</option>
-			<option value="4">Concluído</option>
-			<option value="5">Cancelado</option>
-		</select>
+
+	<div class="col-md-12">
+		<label for="exampleInputEmail1">Solicitado:</label>
+	    <?php echo form_textarea(
+	      'solicitado', 
+	      '',
+	      array( 'class'    =>  'form-control col-md-3',
+	             'required'   =>  'required',
+	             'placeholder'  =>  'Solicitado',
+	            ) 
+	        ); 
+	    ?>
 	</div>
 
-	<div class="form-group">
-	    <label for="exampleInputEmail1">Solução:</label>
+	<div class="col-md-12">
+		<h3>Parte Técnica</h3>
+	</div>
+
+	<div class="col-md-12">
+		<label for="exampleInputEmail1">Detectado:</label>
+	    <?php echo form_textarea(
+	      'detectado', 
+	      '',
+	      array( 'class'    =>  'form-control col-md-3',
+	             'placeholder'  =>  'Detectado',
+	            ) 
+	        ); 
+	    ?>
+	</div>
+
+	<div class="col-md-12">
+		<label for="exampleInputEmail1">Solução:</label>
 	    <?php echo form_textarea(
 	      'solucao', 
 	      '',
@@ -109,20 +129,14 @@
 	    ?>
 	</div>
 
-	<div class="form-group">
-	    <label for="exampleInputEmail1">Observacão:</label>
-	    <?php echo form_textarea(
-	      'observacao', 
-	      '',
-	      array( 'class'    =>  'form-control col-md-3',
-	             'placeholder'  =>  'Observação',
-	            ) 
-	        ); 
-	    ?>
+	
+
+	<div class="col-md-12">
+		<h3>Finalização</h3>
 	</div>
 
-	<div class="form-group">
-	    <label for="exampleInputEmail1">Data de Conclusão:</label>
+	<div class="col-md-2">
+		<label for="exampleInputEmail1">Data de Conclusão:</label>
 	    <?php echo form_input(
 	      'datCon', 
 	      '',
@@ -135,7 +149,7 @@
 	    ?>
 	</div>
 
-	<div class="form-group">
+	<div class="col-md-3">
 		<label for="exampleInputEmail1">Técnico Responsável:</label>
 		<select name="tecRes" id="tecRes" class="form-control">
 			<?php foreach( $usuarios as $usuario ): ?>
@@ -148,8 +162,8 @@
 		</select>
 	</div>
 
-	<div class="form-group">
-	    <label for="exampleInputEmail1">Acréscimo de Valor:</label>
+	<div class="col-md-3">
+		<label for="exampleInputEmail1">Acréscimo de Valor:</label>
 	    <?php echo form_input(
 	      'acrescimo', 
 	      '',
@@ -160,8 +174,8 @@
 	    ?>
 	</div>
 
-	<div class="form-group">
-	    <label for="exampleInputEmail1">Desconto de Valor:</label>
+	<div class="col-md-3">
+		<label for="exampleInputEmail1">Desconto de Valor:</label>
 	    <?php echo form_input(
 	      'desconto', 
 	      '',
@@ -172,25 +186,41 @@
 	    ?>
 	</div>
 
-	<div class="form-group">
-    	<?php echo form_label('Data de Cadastro:', 'date'); ?>
+	<div class="col-md-2">
+		<?php echo form_label('Data de Cadastro:', 'date'); ?>
     	<?php date_default_timezone_set('America/Sao_Paulo'); $date = date('d-m-Y'); ?>
     	<?php echo form_input('datCad', $date ,array( 'class'=>'form-control col-md-3', 'required'=>'required', 'readonly'=>'readonly', 'data-mask' => 'data-mask',
              'data-inputmask' => '\'alias\': \'dd/mm/yyyy\'',  ) ); ?>
-  	</div>
+	</div>
 
-	<div class="form-group">
-    	<label for="exampleInputEmail1">Autor:</label>
-    	<select name="usuCad" id="usuCad" class="form-control" readonly>
-			<option value="<?php echo $infos[0]->id; ?>"><?php echo $infos[0]->nome; ?></option>
-    	</select>
-  	</div>
+	<div class="col-md-3">
+		<div class="form-group">
+	    	<label for="exampleInputEmail1">Autor:</label>
+	    	<select name="usuCad" id="usuCad" class="form-control" readonly>
+				<option value="<?php echo $infos[0]->id; ?>"><?php echo $infos[0]->nome; ?></option>
+	    	</select>
+  		</div>
+	</div>
 
-	<button type="submit" class="btn btn-primary">Cadastrar</button>
+	<div class="col-md-12">
+		<label for="exampleInputEmail1">Observacão:</label>
+	    <?php echo form_textarea(
+	      'observacao', 
+	      '',
+	      array( 'class'    =>  'form-control col-md-3',
+	             'placeholder'  =>  'Observação',
+	            ) 
+	        ); 
+	    ?>
+	</div>
+
+	<div class="col-md-8">
+		<br>
+		<button type="submit" class="btn btn-primary">Cadastrar</button>
+	</div>
 
 <?php form_close(); ?>
 
 
-</div><!--col-md-4-->
 
 </div></div>
